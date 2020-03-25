d50e2152017a586767485c5690c4cdc4affd83e6
Kvvrtv <kvvrtv@users.noreply.github.com>
2020-03-25 05:39:50 +0100

    Update bestaetigung.php

--- offiziel/bestaetigung.php
+++ offiziel/bestaetigung.php
@@ -51,7 +51,6 @@
                    " | Geschlecht: " . $gender. " | Nachname: " . $surname . " | Vorname: " . $firstname . " | Bday: " . $bday . " | Schule: " . $school . " | Klasse: " . $class . " | Niveau: " . $level .
                    " | PLZ: " . $postcode . " | Ort: " . $place . " | Strasse: " . $street . " | Hausnummer: " . $housenumber . " | ZusatzStrasse: " . $additionalstreet . " | Telefon: " . $mobile . " | E-Mail: " . $email;
 
-                  // echo " IDEvent Typ: ", gettype($idEventBestaetigung), "\n";
 
 
                    // Create connection
@@ -77,69 +76,19 @@
 				   echo "</br></br></br>" . $row['freiePlaetze'] . "</br></br></br>";
 
                    if ($row['freiePlaetze'] != "0") {
-                   	// output data of each row
-                   //try{
-                   // else{
-                    /* $statement = $conn->prepare("SELECT freiePlaetze FROM `event` WHERE IDEvent = ?");
-                     $statement->bind_param( "s", $idEventBestaetigung);
-                     $statement->execute();
-                     $resultfreiePlaetze = $statement->get_result();
-                     $value = $resultfreiePlaetze->fetch_field_direct(0);
-                     $resultfreiePlaetze -> free_result();
-                     //strval($resultfreiePlaetze);
-                     $statement->close();
-
-                     echo '</br></br></br>' . $value -> name;
-
-                     $result = $statement->get_result();
-                     echo "freiePlaetze: " . $result;
-
-                     while($row = $result->fetch_assoc()) {
-                       echo $row->freiePlaetze;
-                     }
-                     $query = mysqli_query("SELECT freiePlaetze FROM `event` WHERE IDEvent = " . $idEventBestaetigung);
-                     $row = mysqli_fetch_row($query);
-                     }
-                    catch(PDOException $e) {
-                     echo "Error: " . $e->getMessage();
-                    } */
-                    //if ('freiePlaetze' != "0"){
-                           // prepare sql and bind parameters
-                           /* $stmt = $conn->prepare("INSERT INTO personen (`Geschlecht`, `Nachname`, `Vorname`, `Geburtstag`, `Schule`, `Klasse`, `Niveau`, `PLZ`, `Ort`, `Strasse`, `Hausnummer`, `Zusatz Strasse`, `Telefon`, `E-Mail`)
-                           VALUES (:gender, :surname, :firstname, :bday, :school, :class, :level, :postcode, :place, :street, :housenumber, :additionalstreet, :mobile, :email)"); */
-                           $stmt = $conn->prepare("INSERT INTO personen (`Geschlecht`, `Nachname`, `Vorname`, `Geburtstag`, `Schule`, `Klasse`, `Niveau`, `PLZ`, `Ort`, `Strasse`, `Hausnummer`, `Zusatz Strasse`, `Telefon`, `E-Mail`)
+                                              $stmt = $conn->prepare("INSERT INTO personen (`Geschlecht`, `Nachname`, `Vorname`, `Geburtstag`, `Schule`, `Klasse`, `Niveau`, `PLZ`, `Ort`, `Strasse`, `Hausnummer`, `Zusatz Strasse`, `Telefon`, `E-Mail`)
                                                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
                            $stmt->bind_param("ssssssssssssss", $gender, $surname, $firstname, $bday, $school, $class, $level, $postcode, $place, $street, $housenumber, $additionalstreet, $mobile, $email);
-                           /*$stmt->bindParam(':surname', $surname);
-                           $stmt->bindParam(':firstname', $firstname);
-                           $stmt->bindParam(':bday', $bday);
-                           $stmt->bindParam(':school', $school);
-                           $stmt->bindParam(':class', $class);
-                           $stmt->bindParam(':level', $level);
-                           $stmt->bindParam(':postcode', $postcode);
-                           $stmt->bindParam(':place', $place);
-                           $stmt->bindParam(':street', $street);
-                           $stmt->bindParam(':housenumber', $housenumber);
-                           $stmt->bindParam(':additionalstreet', $additionalstreet);
-                           $stmt->bindParam(':mobile', $mobile);
-                           $stmt->bindParam(':email', $email);*/
-                           $stmt->execute();$stmt->close();
-                       //}
-                       /*catch(PDOException $e) {
-                           echo "Error: " . $e->getMessage();
-                       }*/
 
-                       //try{
+                           $stmt->execute();$stmt->close();
+
                           $neuFreiePlaetzeBestaetigung = $freiePlaetzeBestaetigung - 1;
 
                           $sql = "UPDATE event SET freiePlaetze = " . $neuFreiePlaetzeBestaetigung . " WHERE IDEvent = " . $idEventBestaetigung . "";
 
                           $stmtTwo = $conn->prepare($sql);
                           $stmtTwo->execute();$stmtTwo->close();
-                       //}
-                       /*catch(PDOException $e){
-                            echo "Error2: " . $e->getMessage();
-                       }*/
+
                        ?>
 
                        <!-- end database -->
@@ -250,33 +199,6 @@
                    }
                    $conn->close();
                    ?>
-           		<!-- try {
-           			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-           			// set the PDO error mode to exception
-           			$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING, PDO::ERRMODE_EXCEPTION);
-           			$sql = $conn->prepare("INSERT INTO personen (`Geschlecht`, `Nachname`, `Vorname`, `Geburtstag`, `Schule`, `Klasse`, `Niveau`, `PLZ`, `Ort`, `Strasse`, `Hausnummer`, `Zusatz Strasse`, `Telefon`, `E-Mail`)
-           			VALUES (:gender, :surname, :firstname, :bday, :school, :class, :level, :postcode, :place, :street, :housenumber, :additionalstreet, :mobile, :email)");
-           			$sql->bindParam(':gender', $gender);
-           			$sql->bindParam(':surname', $surname);
-           			$sql->bindParam(':firstname', $firstname);
-           			$sql->bindParam(':bday', $bday);
-           			$sql->bindParam(':school', $school);
-           			$sql->bindParam(':class', $class);
-           			$sql->bindParam(':level', $level);
-           			$sql->bindParam(':postcode', $postcode);
-           			$sql->bindParam(':place', $place);
-           			$sql->bindParam(':street', $street);
-           			$sql->bindParam(':housenumber', $housenumber);
-           			$sql->bindParam(':additionalstreet', $additionalstreet);
-           			$sql->bindParam(':mobile', $mobile);
-           			$sql->bindParam(':email', $email);
-           			// use exec() because no results are returned
-           			$conn->exec($sql);
-           			$conn = null;
-           		}
-           		catch (PDOException $e) {
-           			echo $sql . "<br>" . $e->getMessage();
-           			print_r($sql->errorInfo());
-           		} -->
+
 	</body>
 </html>
